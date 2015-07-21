910958ee2ca98e8be3629e1428a5ec93a4b2c828
Shamsher <ansarisam47@gmail.com>
2015-07-21 20:52:56 +0530

    Add hapi good plugin for logging to console

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var Hapi = require('hapi'),
     Tv = require('tv');
 
 // Create a server with a host and port
-var server = new Hapi.Server();
+var server = new Hapi.Server({ debug: { request: ['error'] } });
 server.connection({ 
   host: 'localhost', 
   port: 8000 
@@ -17,7 +17,20 @@ server.route({
   }
 });
 
-var options = {endpoint: '/logs'};
+var options = {endpoint: '/logs'},
+    reporter = {
+      reporters : [{
+        reporter : require('good-console'),
+        events: { log: '*', response: '*' }
+      }]
+    };
+
+server.register({
+  register : require('good'),
+  options : reporter
+}, function(err) {
+  
+});
 
 server.register({register: Tv, options: options}, function (err) {
   if (!err) {
