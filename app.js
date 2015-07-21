7c7dfd462dc49745ae618690101876a9dc5dfd08
Shamsher <ansarisam47@gmail.com>
2015-07-21 20:36:07 +0530

    Add Hapi TV plugin for browser based logging

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var Hapi = require('hapi');
+var Hapi = require('hapi'),
+    Tv = require('tv');
 
 // Create a server with a host and port
 var server = new Hapi.Server();
@@ -16,5 +17,10 @@ server.route({
   }
 });
 
-// Start the server
-server.start();
+var options = {endpoint: '/logs'};
+
+server.register({register: Tv, options: options}, function (err) {
+  if (!err) {
+    server.start();
+  }
+});
