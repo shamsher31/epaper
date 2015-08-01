eb5cd13138f58e81da02d4fd0b5a0821e1e0fb56
Shamsher <ansarisam47@gmail.com>
2015-08-01 22:03:21 +0530

    Upload image initial code

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var Hapi = require('hapi');
+var Hapi = require('hapi'),
+    fs = require('fs');
 
 require('dotenv').load();
 
@@ -19,7 +20,44 @@ server.route({
   method: 'GET',
   path:'/', 
   handler: function (request, reply) {
-    reply('Urdu Newspaper');
+     reply(
+        '<form action="/upload" method="post" enctype="multipart/form-data">' +
+        '<input type="file" name="fileUpload">' +
+        '<input type="submit" value="Upload">' +
+        '</form>'
+      );
+  }
+});
+
+server.route({
+  path: '/upload',
+  method: 'POST',
+  config: {
+    payload: {
+      maxBytes: 1048576, // 1MB
+      output: 'stream',
+      allow: 'multipart/form-data',
+      parse: true
+    },
+    handler: function (request, reply) {
+
+      // This is the directory you wish to place the files.
+      var uploadDir = './uploads/';
+
+      // Create stream where the files will go.
+      var writeStream = fs.createWriteStream(uploadDir + request.payload.fileUpload.hapi.filename);
+
+      // Pipe the payload file into the write stream.
+      request.payload.fileUpload.pipe(writeStream);
+
+      // On stream end or error send a response.
+      request.payload.fileUpload.on('end', function(){
+        reply({"Status":"Done"});
+      }).on('error', function(){
+        reply('Error in uploading');
+      });
+
+    }
   }
 });
 
